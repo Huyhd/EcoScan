71b72fa8a5f8e06963b79d984dcb0159a7f17448
nhoxbypass <nhoxbypass@gmail.com>
2018-09-14 00:55:45 +0700

    Refactor project structure to modern architecture

---
+++ app/src/main/java/app/creatingminds/ecoscan/SettingActivity.java
@@ -0,0 +1,36 @@
+package app.creatingminds.ecoscan;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+
+public class SettingActivity extends PreferenceActivity implements Preference.OnPreferenceClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_setting);
+        getActionBar().setTitle("설정");
+
+        Preference pAppName = (Preference)findPreference("keyhelp");
+        Preference pAppVersion = (Preference)findPreference("keycontact");
+
+        pAppName.setOnPreferenceClickListener(this);
+        pAppVersion.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference)
+    {
+        // 도움말 선택시
+        if(preference.getKey().equals("keyhelp"))
+        {
+            //intent will be in here
+        }
+        else if(preference.getKey().equals("keycontact"))
+        {
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/app/creatingminds/ecoscan/SplashActivity.java
@@ -0,0 +1,26 @@
+package app.creatingminds.ecoscan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler splashhandler = new Handler();
+        splashhandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(SplashActivity.this, MainActivity.class);
+
+                startActivity(splash);
+                finish();
+            }
+        }, 2000);
+    }
+}
