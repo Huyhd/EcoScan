6e72d1b118f958960684f9bd6c7635b8a5a6761f
nhoxbypass <nhoxbypass@gmail.com>
2018-09-15 20:59:52 +0700

    Fix formatter

--- app/src/main/java/app/creatingminds/ecoscan/utils/FormatUtils.java
+++ app/src/main/java/app/creatingminds/ecoscan/utils/FormatUtils.java
@@ -9,7 +9,7 @@ import java.util.Locale;
  * Created by tom on 9/15/18.
  */
 public final class FormatUtils {
-    private static final DateFormat NORMAL_DATE_FORMATTER = new SimpleDateFormat("yyyy/mm/dd", Locale.US);
+    private static final DateFormat NORMAL_DATE_FORMATTER = new SimpleDateFormat("yyyy//MM/dd", Locale.US);
 
     public static String formatDate(long timestamp) {
         return NORMAL_DATE_FORMATTER.format(timestamp);
