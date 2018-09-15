c79fc9e220255351f0a3c2a14e9e08a0d9ba5069
nhoxbypass <nhoxbypass@gmail.com>
2018-09-16 00:24:54 +0700

    Fix navigation view & crash in settings screen

--- app/src/main/java/app/creatingminds/ecoscan/ui/settings/SettingActivity.java
+++ app/src/main/java/app/creatingminds/ecoscan/ui/settings/SettingActivity.java
@@ -11,27 +11,24 @@ public class SettingActivity extends PreferenceActivity implements Preference.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.layout.activity_setting);
 
-        setContentView(R.layout.activity_setting);
-        getActionBar().setTitle("설정");
+        if (getActionBar() != null)
+            getActionBar().setTitle("설정");
 
-        Preference pAppName = (Preference)findPreference("keyhelp");
-        Preference pAppVersion = (Preference)findPreference("keycontact");
+        Preference pAppName = findPreference("keyhelp");
+        Preference pAppVersion = findPreference("keycontact");
 
         pAppName.setOnPreferenceClickListener(this);
         pAppVersion.setOnPreferenceClickListener(this);
     }
 
     @Override
-    public boolean onPreferenceClick(Preference preference)
-    {
+    public boolean onPreferenceClick(Preference preference) {
         // 도움말 선택시
-        if(preference.getKey().equals("keyhelp"))
-        {
+        if (preference.getKey().equals("keyhelp")) {
             //intent will be in here
-        }
-        else if(preference.getKey().equals("keycontact"))
-        {
+        } else if (preference.getKey().equals("keycontact")) {
         }
         return false;
     }
